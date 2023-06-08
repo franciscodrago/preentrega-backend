8991ccdc8f06a48f2241ec7f9b69a38ac4030515
francisco drago <franciscodrago1999@gmail.com>
2023-06-08 11:45:34 -0300

    preentrega

---
+++ src/managers/ProductManager.js
@@ -0,0 +1,133 @@
+import fs from "fs"
+
+class ProductManager{
+    
+    #path = ""
+
+    constructor(path){
+        this.#path = path
+    }
+    
+    //Obtengo lista de productos
+    async getProducts(){
+        try{
+            const products = await fs.promises.readFile(this.#path, "utf-8")
+            return JSON.parse(products)
+        }catch(err){
+            console.log(err)
+            return []
+        }
+    }
+
+    //genera un array con los Id's
+    async getIDs(){
+        let products = await this.getProducts()
+        let ids = products.map( prods => prods.id)
+        let mayorID = Math.max(...ids)
+        if (mayorID === -Infinity) {
+            return 0
+        } else {
+            return ++mayorID
+        }
+    }
+    
+    //Agregar el producto
+    async addProduct(title, description, price, thumbail, code, stock, status, category){
+
+        try{
+            let mayorID = await this.getIDs()
+            
+            const product = {
+                id : mayorID,
+                title,
+                description,
+                price,
+                thumbail,
+                code,
+                stock,
+                status,
+                category
+            }
+
+            let products = await this.getProducts()
+            let verificar = Object.values(product)
+            let sameCode = products.find( prod => prod.code === code)
+
+            if (verificar.includes(undefined)){
+                throw new Error(`El producto ${product.title} NO ha sido cargado, debe completar todos los datos.`)
+            }
+            if(sameCode){
+                throw new Error(`El producto ${product.title} NO ha sido cargado ya que la propiedad "code" está repetida, ${sameCode.title} tiene el mismo valor.`)
+            }
+        
+            products = [...products, product]
+            console.log(`${product.title} cargado correctamente.`)
+            await fs.promises.writeFile(this.#path, JSON.stringify(products))
+        }catch(err){
+            throw new Error(err)
+        }
+        
+    }
+
+    //Actualiza el producto
+    async updateProduct(id, propModify){
+        let products = await this.getProducts()
+        let productModify = products.find(i => i.id === id)
+
+        if (!productModify){
+            throw new Error('No se encontró ningún producto con ese ID.')
+        }
+
+        if (Object.keys(propModify).includes('id')){
+            throw new Error('No es posible modificar el ID de un producto.')
+        }
+
+        if (Object.keys(propModify).includes('code')){
+            let sameCode = products.some(i => i.code === propModify.code)
+            if (sameCode){
+                throw new Error('No es posible modificar la propiedad code por una que ya exista.')
+            }
+        }
+
+        if (Object.keys(propModify).includes('price')){
+            propModify.price = parseInt(propModify.price)
+        }
+
+        if (Object.keys(propModify).includes('stock')){
+            propModify.stock = parseInt(propModify.stock)
+        }
+        
+        productModify = {...productModify, ...propModify}
+        let newArray = products.filter( prods => prods.id !== id)
+        newArray = [...newArray, productModify]
+        await fs.promises.writeFile(this.#path, JSON.stringify(newArray))
+        console.log('Modificación realizada con éxito.')
+    }
+
+    //filtra y obtiene el producto por Id
+    async getProductById(id){
+        let products = await this.getProducts()
+        let element = products.find(elem => elem.id === id)
+        if (element){
+            return element
+        } else {
+            throw new Error('No se encuentra producto con ese ID.')
+        }
+    }
+
+    //Borra el producto por Id
+    async deleteProduct(id){
+        let products = await this.getProducts()
+        let check = products.some(prod => prod.id === id)
+        
+        if (!check){
+            throw new Error('No existe producto con ese ID.')
+        }
+
+        let newArray = products.filter(prods => prods.id !== id)
+        await fs.promises.writeFile(this.#path, JSON.stringify(newArray))
+        console.log('Producto eliminado con éxito')
+    }
+}
+
+export default ProductManager
